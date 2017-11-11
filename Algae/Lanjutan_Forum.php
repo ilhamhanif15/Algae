44d3eafa76f265f87f576b335a28278bab7a4db9
ilhamhanif15 <ilhamhanif1512@gmail.com>
2017-11-11 23:52:09 +0700

    Update Lanjutan_Forum.php

--- Algae/Lanjutan_Forum.php
+++ Algae/Lanjutan_Forum.php
@@ -51,36 +51,182 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-    <button type="button" class="fa fa-angle-double-left" data-toggle="modal" data-target="Forum.php" style="padding-top: 4px;padding-bottom: 4px;padding-right: 20px;margin-right:7px;margin-top:15px;"> Kembali</button>
+    <button type="button" class="btn btn-primary fa fa-angle-double-left" data-toggle="modal" data-target="Forum.php" style="padding-top: 4px;padding-bottom: 4px;padding-right: 20px;margin-right:7px;margin-top:15px;"> Kembali</button>
+      <ol class="breadcrumb pull-right">
+        <li><a href="#"><i class="fa fa-home" style="color: green"></i> Home</a></li>
+        <li><a href="#">Forum</a></li>
+        <li class="active">[Algorithm Problem's] Brute Force Problem ???</li>
+      </ol>
     </section>
 
     <section class="content">
       <div class="row">
         <div class="col-md-12">
-     <div class="box box-solid box-success">
-            <div class="box-header bg-green">
-              <h3 class="box-title">[Algorithm Problem's] Brute Force Problem ???</h3>
-            </div>
-            <div class="box-footer no-padding">
-              <ul class="nav nav-pills nav-stacked">
-                <li><a href="#">[Algorithm Problem's] Brute Force Problem ??? <br/>
-                 <a href="#" style="text-align: justify;">Algoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya.
-                 </a> 
-                </a>
-                </li>
+         <div class="box box-solid box-success">
+                <div class="box-header bg-green">
+                  <h3 class="box-title">Forum Diskusi</h3>
+                </div>
+                <div class="box-body">
+                    <h3 style="margin-top:3px;">[Algorithm Problem's] Brute Force Problem ???
+                    </h3>
+                    <p class="text-muted">posted on 25 October 2017, 13:20 AM
+                      <span class="label label-success">Advance Algorithm</span>
+                      <span class="label label-primary">Brute Force</span>
+                    </p>
+                    <p class="text-muted">
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star"></span>
+                      <span class="fa fa-star"></span>
+                      based on user's rating
+                    </p>
+                    <hr/>
+                    <div class="row">
+                      <div class="col-md-2" style="text-align: center;">
+                        <div>
+                          <img class="img-circle" style="width:90px;height: 90px;" src="dist/img/avatar04.png" alt="User Avatar">
+                        </div><hr style="margin-top: 10px;margin-bottom: 10px;" />
+                        <div style="margin-left: 10px;">
+                          <p class="text-muted">Author: <a href="">Dias Pambudi</a>  
+                          </p>
+                          <span>
+                            <span class="label label-primary">Lv. 50</span>
+                            <span class="label label-success">Senior User</span>
+                            <span class="label label-danger">Expert</span>
+                          </span>
+                          <hr/>
+                        </div>
+                      </div>
+                      <div class="col-md-10">
+                        <div class="pull-left" style="margin-top: 10px;">
+                        <p style="text-align: justify;">Algoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya. lgoritma brute force merupakan algoritma pencocokan string yang ditulis tanpa memikirkan peningkatan performa. Algoritma ini sangat jarang dipakai dalam praktik, namun berguna dalam studi pembanding dan studi-studi lainnya.
+                         </p>
+                         <hr/>
+                         <h4>Lampiran</h4>
+                         <img class="img-responsive pad" src="dist/img/photo2.png" alt="Photo" style="width: 80%">
+                     </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.footer -->
+              </div>
+        </div>
+        <div class="col-md-12">
+          <h3 style="margin-top: 0px;">Kolom Komentar (2 Komentar)</h3>
+        </div>
+
+        <div class="col-md-12">
+          <!-- Box Comment -->
+          <div class="box box-solid box-primary">
+            <!-- /.box-header -->
+            <div class="box-body">
+            <p class="pull-right" style="margin-right: 20px;margin-top: 9px;font-size: 20px;font-weight: bold;color: green;">+1</p>
+              <div class="user-block">
+                <img class="img-circle" src="dist/img/user1-128x128.jpg" alt="User Image">
+                <span class="username"><a href="#">Jonathan Burke Jr.</a></span>
+                <span class="description">
+                  <span class="label label-primary">Lv. 0</span>
+                  <span class="label label-success">Administrator</span>
+                </span>
+                <span class="description">Comment on 25 Oct 2017, 7:30 PM</span>
                 
-                <li><a href="#" class="text-blue" style="text-align: center">Baca lebih lanjut <span class="fa fa-angle-double-right"></span></a>
-                </li>
-              </ul>
+              </div>
+
+              <hr/>
+              <!-- post text -->
+              <p>Far far away, behind the word mountains, far from the
+                countries Vokalia and Consonantia, there live the blind
+                texts. Separated they live in Bookmarksgrove right at</p>
+
+              <p>the coast of the Semantics, a large language ocean.
+                A small river named Duden flows by their place and supplies
+                it with the necessary regelialia. It is a paradisematic
+                country, in which roasted parts of sentences fly into
+                your mouth.</p>
+
+              <!-- Social sharing buttons -->
+              <span class="pull-right">
+                <button type="button" class="btn btn-primary btn-xs"><i class="fa fa-thumbs-up"></i> Like</button>
+                <button type="button" class="btn btn-danger btn-xs"><i class="fa fa-thumbs-down"></i> Dislike</button>
+                <button type="button" title="Report" class="btn btn-danger btn-xs"><i class="fa fa-flag"></i></button>
+              </span>
             </div>
-            <!-- /.footer -->
+            <!-- /.box-footer -->
           </div>
+          <!-- /.box -->
+        </div>
+
+        <div class="col-md-12">
+          <!-- Box Comment -->
+          <div class="box box-solid box-primary">
+            <!-- /.box-header -->
+            <div class="box-body">
+            <span class="pull-right">
+              <p style="margin-right: 20px;margin-top: 9px;font-size: 20px;font-weight: bold;color: red;">-1</p>
+            </span>
+              <div class="user-block">
+                <img class="img-circle" src="dist/img/user1-128x128.jpg" alt="User Image">
+                <span class="username"><a href="#">Jonathan Burke Jr. II</a></span>
+                <span class="description">
+                  <span class="label label-primary">Lv. 5</span>
+                  <span class="label label-success">Newbie</span>
+                </span>
+                <span class="description">Comment on 25 Oct 2017, 7:30 PM</span>
+                
+              </div>
+
+              <hr/>
+              <!-- post text -->
+              <p>Far far away, behind the word mountains, far from the
+                countries Vokalia and Consonantia, there live the blind
+                texts. Separated they live in Bookmarksgrove right at</p>
+
+              <p>the coast of the Semantics, a large language ocean.
+                A small river named Duden flows by their place and supplies
+                it with the necessary regelialia. It is a paradisematic
+                country, in which roasted parts of sentences fly into
+                your mouth.</p>
+
+              <!-- Social sharing buttons -->
+              <span class="pull-right">
+                <button type="button" class="btn btn-primary btn-xs"><i class="fa fa-thumbs-up"></i> Like</button>
+                <button type="button" class="btn btn-danger btn-xs"><i class="fa fa-thumbs-down"></i> Dislike</button>
+                <button type="button" title="Report" class="btn btn-danger btn-xs"><i class="fa fa-flag"></i></button>
+              </span>
+            </div>
+            <!-- /.box-footer -->
           </div>
+          <!-- /.box -->
+        </div>
+
+        <div style="text-align: center;">
+          <ul class="pagination">
+            <li class="active"><a href="#">1</a></li>
+            <li class=""><a href="#">2</a></li>
+          </ul> 
+        </div>
+
+        <div class="col-md-12"> 
+          <div class="box box-solid box-success">
+            <div class="box-body">
+              <div class="row">
+                <div class="col-md-1" style="text-align: center;">
+                  <img class="img-circle" src="dist/img/user1-128x128.jpg" style="width:80px;height: 80px;" alt="User Image">
+                </div>
+                <div class="col-md-11">
+                  <div class="form-group">
+                    <label>Komentar</label>
+                    <textarea class="form-control" rows="3" placeholder="Ketik Disini..." style="max-width: 100%"></textarea>
+                  </div>
+                  <button class="btn btn-primary pull-right">Submit</button>
+                </div>
+              </div>
+            </div>
           </div>
-            Komentar    : <br>
-            <textarea name="komentar" cols="50" rows="5"> </textarea><br>
-            <input type="submit" value="Kirim">
-            <input type="reset" value="Batal">
+        </div>
+
+      </div>
     </div>
   </section>
 
